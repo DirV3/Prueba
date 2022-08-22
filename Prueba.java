62e1d5011cc1199dc8427146ca30b7668d2ce747
DirV3 <110427898+DirV3@users.noreply.github.com>
2022-08-22 13:00:05 -0600

    Add files via upload

---
+++ Prueba.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.prueba;
+
+public class Prueba {
+ 
+ public int modeloVehiculo;
+ 
+ public String nombreVehiculo="";
+ 
+ public String transportar(){
+  return "Metodo transportar de clase Vehiculo";
+ }
+}
+
